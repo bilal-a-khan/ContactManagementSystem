ae20e23f6ddcc40c12b867dd1b76b0b5c843f027
bilal-a-khan <142792067+bilal-a-khan@users.noreply.github.com>
2023-09-19 09:08:07 +0100

    Add JUnit dependencies and UnitTests Class

---
+++ src/test/java/MainUnitTests.java
@@ -0,0 +1,29 @@
+import org.example.Main;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+public class MainUnitTests {
+
+    Main uut;
+
+    @BeforeEach
+    public void setUp() {
+        uut = new Main();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        uut = null;
+    }
+
+    @Test
+    public void mainUnitTest() {
+        Main.main();
+
+    }
+
+
+}
